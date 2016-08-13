7aa48b30066a9cf6870796f4566f336cf997b327
fenwick <dfharwell@gmail.com>
2016-08-13 21:20:31 +0000

    make more progress

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ var express = require('express')
 var passport = require('passport');
 var Models = require('./lib/models');
 var templates = require('./lib/templates');
+var bodyParser = require('body-parser');
 
 var app = express();
 
@@ -14,14 +15,14 @@ app.use(serveStatic('pages'));
 
 app.listen(process.env.PORT || 3000);
 
-app.get('/:pagename',function(req,res){
+app.get('/wiki/:pagename',function(req,res){
   if (!req.params.pagename){
     return fourOhFour(res);
   }
-  Models.Page.findOne({title:req.params.pagename},function(er,page){
+  Models.Article.findOne({title:req.params.pagename},function(er,article){
     if (er){return genericError(er,res);}
-    if (!page){return fourOhFour(res);}
-    templates.renderPage(page,function(er,html){
+    if (!article){return fourOhFour(res);}
+    templates.renderArticle(article,function(er,html){
       if(er){return genericError(er,res);}
       res.status(200);
       return res.send(html);
@@ -37,6 +38,30 @@ app.get('/',function(req,res){
   });
 });
 
+app.get('/setup',function(req,res){
+  templates.renderSetup(function(er,html){
+    if(er){return genericError(er,res);}
+    res.status(200);
+    return res.send(html);
+  });
+});
+
+var bpm = bodyParser.urlencoded({ extended: false });
+
+app.post('/setup',bpm,function(req,res){
+  //do something
+  Models.Wiki.findOne({},function(er,wiki){
+    if (er || !wiki) return genericError(er,res);
+    wiki.title = req.body.title;
+    wiki.auth = false;
+    wiki.setUp = true;
+    wiki.save(function(er){
+      if (er) return genericError(er,res);
+      return res.redirect('/');
+    });
+  });
+});
+
 function fourOhFour(res){
   res.status(404);
   res.send('not found ;(');
@@ -44,7 +69,8 @@ function fourOhFour(res){
 
 function genericError(er,res){
   res.status(500);
-  res.send(er||'internal server error');
+  res.send(er?er.toString():'internal server error');
+  if(er){console.error('handled error: ',er)}
 }
 
 console.log('listening on '+(process.env.PORT || 3000));
